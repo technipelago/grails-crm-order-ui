0f4ec7c69f427d7e8143db885772742b22f3b042
Goran Ehrsson <goran@technipelago.se>
2013-10-22 13:19:19 +0200

    Version 1.2.0

--- CrmOrderUiGrailsPlugin.groovy
+++ CrmOrderUiGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmOrderUiGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.0-SNAPSHOT"
+    def version = "1.2.0"
     def grailsVersion = "2.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
