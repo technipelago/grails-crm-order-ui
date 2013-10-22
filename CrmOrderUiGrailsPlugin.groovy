49a28488876a7336abdc2c4dc19d4afa55102a12
Goran Ehrsson <goran@technipelago.se>
2013-10-22 12:39:15 +0200

    Cleanup

--- CrmOrderUiGrailsPlugin.groovy
+++ CrmOrderUiGrailsPlugin.groovy
@@ -1,43 +1,39 @@
+/*
+ * Copyright (c) 2013 Goran Ehrsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 class CrmOrderUiGrailsPlugin {
-    // Dependency group
     def groupId = "grails.crm"
-    // the plugin version
-    def version = "1.0.3"
-    // the version or versions of Grails the plugin is designed for
+    def version = "1.2.0-SNAPSHOT"
     def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
-
     def title = "CRM Order Management UI"
     def author = "Göran Ehrsson"
     def authorEmail = "goran@technipelago.se"
     def description = '''\
 Grails CRM Order Management User Interface
 '''
-
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/crm-order-ui"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
     def organization = [name: "Technipelago AB", url: "http://www.technipelago.se/"]
 
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def issueManagement = [system: "github", url: "https://github.com/goeh/grails-crm-order-ui/issues"]
+    def scm = [url: "https://github.com/goeh/grails-crm-order-ui"]
 
     def features = {
         crmOrderUi {
