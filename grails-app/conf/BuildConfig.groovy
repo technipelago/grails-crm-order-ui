23e45ed28eed04975247fe81dc0866c3b6b1106a
Paul Waldo <paulwwaldo@gmail.com>
2015-05-14 15:05:56 -0400

    Upgrade to grails 2.4.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,35 +2,35 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-    inherits("global") {}
-    log "warn"
-    legacyResolve false
-    repositories {
-        grailsCentral()
-        mavenCentral()
-    }
-    dependencies {
-    }
+	inherits("global") {}
+	log "warn"
+	legacyResolve false
+	repositories {
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
+	}
 
-    plugins {
-        runtime ":hibernate:$grailsVersion"
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
-            export = false
-        }
-        test(":spock:0.7") { export = false }
-        test(":codenarc:0.21") { export = false }
+	plugins {
+		build ":tomcat:7.0.55"
+		build ":release:3.0.1"
+		runtime (":hibernate4:4.3.6.1") {
+			excludes "net.sf.ehcache:ehcache-core"  // remove this when http://jira.grails.org/browse/GPHIB-18 is resolved
+			export = false
+		}
+
+		test(":codenarc:0.22") { export = false }
 
         compile ":decorator:1.1"
         compile ":user-tag:0.6"
         compile ":selection:0.9.8"
 
-        compile ":crm-core:2.0.2"
-        compile ":crm-security:2.0.0"
-        compile ":crm-ui-bootstrap:2.0.0"
-        compile ":crm-tags:2.0.2"
-        compile ":crm-order:2.0.0"
-    }
+        compile ":crm-core:2.4.0"
+        compile ":crm-security:2.4.0"
+        compile ":crm-ui-bootstrap:2.4.0"
+        compile ":crm-tags:2.4.0"
+        compile ":crm-order:2.4.0-SNAPSHOT"
+	}
 }
