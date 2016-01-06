baf9974b1b3df6cffc5bd55ca4599c555890d770
Goran Ehrsson <goran@technipelago.se>
2016-01-06 02:16:16 +0100

    Version 2.4.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,31 +4,37 @@ grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-	inherits("global") {}
-	log "warn"
-	legacyResolve false
-	repositories {
-		grailsCentral()
-		mavenLocal()
-		mavenCentral()
-	}
+    inherits("global") {}
+    log "warn"
+    legacyResolve false
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
+    dependencies {
+        // See https://jira.grails.org/browse/GPHIB-30
+        test("javax.validation:validation-api:1.1.0.Final") { export = false }
+        test("org.hibernate:hibernate-validator:5.0.3.Final") { export = false }
+    }
+    plugins {
+        build(":release:3.0.1",
+                ":rest-client-builder:1.0.3") {
+            export = false
+        }
+        test(":hibernate4:4.3.6.1") {
+            export = false
+        }
 
-	plugins {
-		build ":tomcat:7.0.55"
-		build ":release:3.0.1"
-		runtime (":hibernate4:4.3.6.1") {
-			excludes "net.sf.ehcache:ehcache-core"  // remove this when http://jira.grails.org/browse/GPHIB-18 is resolved
-			export = false
-		}
-
-		test(":codenarc:0.22") { export = false }
+        test(":codenarc:0.22") { export = false }
+        test(":code-coverage:2.0.3-3") { export = false }
 
         compile ":decorator:1.1"
         compile ":user-tag:1.0"
         compile ":selection:0.9.8"
 
-		compile ":crm-order:2.4.1"
-		compile ":crm-security:2.4.1"
-        compile ":crm-ui-bootstrap:2.4.0"
-	}
+        compile ":crm-order:2.4.2"
+        compile ":crm-security:2.4.2"
+        compile ":crm-ui-bootstrap:2.4.1"
+    }
 }
